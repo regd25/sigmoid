307e8385c905a091552bfb347e27c02175f0396e
Enrique de Jesus <quiquejesus94@gmail.com>
2019-10-12 01:37:40 -0500

    Se termino el slider

--- js/slider.js
+++ js/slider.js
@@ -1,28 +1,3 @@
-/* var sickPrimary = {
-    autoplay: true,
-    autoplaySpeed: 1400,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 1800,
-    asNavFor: '.text-slider',
-    centerMode: true,
-    prevArrow: $('.prev'),
-    nextArrow: $('.next')
-}
-var sickSecondary = {
-    autoplay: true,
-    autoplaySpeed: 1400,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 1800,
-    asNavFor: '.slider',
-    prevArrow: $('.prev'),
-    nextArrow: $('.next')
-}
-$('.slider').slick(sickPrimary);
-$('.text-slider').slick(sickSecondary); */
-
-
 var swiper = new Swiper('.swiper-container', {
     centeredSlides: true,
     effect: 'coverflow',
